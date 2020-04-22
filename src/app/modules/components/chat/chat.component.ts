5461673a360c1841fef1cd237da8aec25d16f5a7
Andrey Rechkov <andrey_rechkov@bitbucket.org>
2020-04-22 20:00:30 +0300

    Save verstka

--- src/app/modules/components/chat/chat.component.ts
+++ src/app/modules/components/chat/chat.component.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { CometChat } from '@cometchat-pro/chat';
-import { environment } from '../../../../environments/environment';
-import {AuthService} from '../../../shared/service/auth.service';
-=======
-import {WebsocketService} from '../../../shared/service/websocket.service';
->>>>>>> Angular-Django-Chat
+import { WebsocketService } from '../../../shared/service/websocket.service';
 
 @Component({
   selector: 'app-chat',
@@ -15,24 +8,12 @@ import {WebsocketService} from '../../../shared/service/websocket.service';
 })
 export class ChatComponent implements OnInit {
 
-<<<<<<< HEAD
-  constructor(private snackBar: MatSnackBar,
-              private auth: AuthService
-              ) {
-    this.init(environment.appId);
-  }
-
-  ngOnInit() {
-    this.auth.getUser().subscribe(res => {
-      console.log(res);
-    });
-=======
   public message: string = '';
 
   constructor(private webSocket: WebsocketService) {}
 
   ngOnInit(): void {
-    this.webSocket.connect();
+    //this.webSocket.connect();
   }
 
   public sendMessage() {
@@ -42,18 +23,5 @@ export class ChatComponent implements OnInit {
       'from': 'firstep'
     }));
     this.message = '';
->>>>>>> Angular-Django-Chat
-  }
-
-  public init(appId: string, apiKey?  : string) {
-    CometChat.init(appId).then(
-      msg => console.log('Initialized succesfull: ', msg),
-      err => {
-        console.log('App init failed', err);
-        this.snackBar.open(
-          'App initialization failed. Please refresh the page.'
-        );
-      }
-    );
   }
 }
